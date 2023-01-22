9e3cfebb0afd7bff427c34c613de2219d782db7c
taemath <hirotaka081630@gmail.com>
2023-01-23 05:26:17 +0900

    feat(everyday-rspec/04-factory): ファクトリで関連を扱う

--- everyday-rspec/spec/factories/users.rb
+++ everyday-rspec/spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:owner] do
     first_name { "Aaron" }
     last_name { 'Sumner' }
-    email { 'teste@example.com' }
     sequence(:email) { |n| "tester#{n}@example.com"}
     password { 'dottle-nouveau-pavilion-tights-furze' }
   end
