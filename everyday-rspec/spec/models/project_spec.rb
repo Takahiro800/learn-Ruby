cd84b2abe29d3360b622a18e5bef7f7a055f3810
taemath <hirotaka081630@gmail.com>
2023-01-23 06:07:31 +0900

    feat(everyday-rspec/04-factory): 複数のデータをFactoryBotで作成する

--- everyday-rspec/spec/models/project_spec.rb
+++ everyday-rspec/spec/models/project_spec.rb
@@ -48,4 +48,28 @@ RSpec.describe Project, type: :model do
 
     expect(other_project).to be_valid
   end
+
+  # 遅延ステータス
+  describe "late status" do
+    # 締切日が過ぎていれば遅延していること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project_due_yesterday)
+
+      expect(project).to be_late
+    end
+
+    # 締切日が今日なら遅延していないこと
+    it "is on time when the due date is today" do
+      project = FactoryBot.create(:project_due_today)
+
+      expect(project).to_not be_late
+    end
+
+    # 締切日が明日なら遅延していないこと
+    it "is on time when the due date is tommorow" do
+      project = FactoryBot.create(:project_due_tomorrow)
+
+      expect(project).to_not be_late
+    end
+  end
 end
