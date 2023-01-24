72c69e6b5282984bb3d466518e76b62062c7ff32
taemath <hirotaka081630@gmail.com>
2023-01-25 05:46:04 +0900

    refactor(everyday-rspec/04-models): 演習問題

--- everyday-rspec/spec/models/project_spec.rb
+++ everyday-rspec/spec/models/project_spec.rb
@@ -9,12 +9,7 @@ RSpec.describe Project, type: :model do
 
   # ユーザー単位では、　重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'test@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze',
-    )
+    user = FactoryBot.create(:user)
 
     user.projects.create(
       name: "Test project",
@@ -30,24 +25,16 @@ RSpec.describe Project, type: :model do
 
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
-    user = User.create(
-      first_name: 'Joe',
-      last_name: 'Tester',
-      email: 'test@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze',
-    )
 
+    user = FactoryBot.create(:user)
     user.projects.create(
       name: "Test Project",
     )
 
-    other_user = User.create(
-      first_name: 'Jane',
-      last_name: 'Tester',
+    other_user = FactoryBot.create(
+      :user,
       email: 'janetester@example.com',
-      password: 'dottle-nouveau-pavilion-tights-furze',
     )
-
     other_project = other_user.projects.build(
       name: "Test Project"
     )
