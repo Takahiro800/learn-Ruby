99327637747ae41ceea3dab451edbe42bbbced85
taemath <hirotaka081630@gmail.com>
2023-01-26 07:01:05 +0900

    feat(everyday-rspec/05-controllers): ユーザー入力のエラーをテストする

--- everyday-rspec/spec/controllers/projects_controller_spec.rb
+++ everyday-rspec/spec/controllers/projects_controller_spec.rb
@@ -81,14 +81,30 @@ RSpec.describe ProjectsController, type: :controller do
         @user = FactoryBot.create(:user)
       end
 
-      # プロジェクトを追加できること
-      it 'adds a project' do
-        project_params = FactoryBot.attributes_for(:project)
-        sign_in(@user)
+      # 有効な属性の場合
+      context 'with valid attributes' do
+        # プロジェクトを追加できること
+        it 'adds a project' do
+          project_params = FactoryBot.attributes_for(:project)
+          sign_in(@user)
 
-        expect{
-          post :create, params: { project: project_params}
-        }.to change(@user.projects, :count).by(1)
+          expect{
+            post :create, params: { project: project_params}
+          }.to change(@user.projects, :count).by(1)
+        end
+      end
+
+      # 無効な属性の場合
+      context 'with invalid attributes' do
+        # プロジェクトを追加できないこと
+        it 'does not add a project' do
+          project_params = FactoryBot.attributes_for(:project, :invalid)
+          sign_in(@user)
+
+          expect {
+            post :create, params: { project: project_params }
+          }.to_not change(@user.projects, :count)
+        end
       end
     end
 
